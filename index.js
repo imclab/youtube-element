b2bca311fb4563861b45d3affdcb3ccbbce07abc
Lance Pollard <lancejpollard@gmail.com>
2013-11-03 19:22:28 -0800

    init

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ var youtube = require('youtube-player');
  */
 
 element('youtube')
-	.attr('src')
-	.attr('width', 'integer')
-	.attr('height', 'integer')
-	.attr('playlist', 'array')
-	.on('render', function(el, scope, attrs){
-		var sel = el.getAttribute('id');
+  .attr('src')
+  .attr('width', 'integer')
+  .attr('height', 'integer')
+  .attr('playlist', 'array')
+  .on('render', function(el, scope, attrs){
+    var sel = el.getAttribute('id');
 
-		var opts = {
-			width: el.getAttribute('width'),
-			height: el.getAttribute('height'),
-			videoId: el.getAttribute('src')
-		};
+    var opts = {
+      width: el.getAttribute('width'),
+      height: el.getAttribute('height'),
+      videoId: el.getAttribute('src')
+    };
 
-		youtube(sel, opts, function(){
-			
-		});
-	});
+    youtube(sel, opts, function(){
+      
+    });
+  });
 
 /**
  * Expose `youtube` element.
